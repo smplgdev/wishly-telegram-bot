c7c47b4f0da0b24775cf9ba3cf2c1abdced0e566
Ivan Mikhailov <smplgdev@air.local>
2023-12-11 12:40:18 +0100

    added set_scheduled_jobs function

--- bot/utils/scheduled_jobs/set_scheduled_jobs.py
+++ bot/utils/scheduled_jobs/set_scheduled_jobs.py
@@ -1,6 +1,8 @@
+from apscheduler.jobstores.redis import RedisJobStore
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 
+from bot.config_reader import config
 from bot.utils.scheduled_jobs.make_wishlists_inactive import make_old_wishlists_inactive
 from bot.utils.scheduled_jobs.message_party_is_soon import party_is_soon_message
 
@@ -27,3 +29,13 @@ def set_scheduled_jobs(
         CronTrigger(hour=15, minute=35),
         misfire_grace_time=ONE_HOUR_IN_SECONDS,
     )
+
+
+if __name__ == '__main__':
+    jobstores = {
+        'default': RedisJobStore(jobs_key='dispatched_trips_jobs', run_times_key='dispatched_trips_running',
+                                 host=config.REDIS_HOST, port=config.REDIS_PORT)
+    }
+    scheduler = AsyncIOScheduler(jobstores=jobstores, timezone='Europe/Berlin')
+    set_scheduled_jobs(scheduler)
+    print("Scheduled jobs were added successfully")
