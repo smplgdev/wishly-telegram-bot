26505385fa00bd17786ded89a3671e757a450484
Ivan <smplgdev@gmail.com>
2023-12-11 22:26:27 +0100

    Fixed error handler

--- bot/handlers/errors/error_handler.py
+++ bot/handlers/errors/error_handler.py
@@ -1,15 +1,15 @@
 import logging
 
-from aiogram import Router
-from aiogram.types import ErrorEvent
+from aiogram import Router, F
+from aiogram.types import ErrorEvent, Message
 
 from bot import strings
 
 router = Router()
 
 
-@router.error()
-async def error_handler(event: ErrorEvent):
+@router.error(F.update.message.as_("message"))
+async def error_handler(event: ErrorEvent, message: Message):
     logging.error("Critical error caused by %s", event.exception, exc_info=True)
 
-    await event.update.message.answer(strings.error_happened)
+    await message.answer(strings.error_happened)
