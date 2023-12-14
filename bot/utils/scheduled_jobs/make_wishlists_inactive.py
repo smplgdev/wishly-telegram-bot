1b4c8b9fcda6e1fc07741a4236798eb949f100f7
Ivan <smplgdev@gmail.com>
2023-12-14 05:35:07 +0100

    Fixed a bug with closed session in make_old_wishlists_inactive function

--- bot/utils/scheduled_jobs/make_wishlists_inactive.py
+++ bot/utils/scheduled_jobs/make_wishlists_inactive.py
@@ -9,5 +9,6 @@ async def make_old_wishlists_inactive(date_: datetime.date = datetime.date.today
     async with get_async_session().begin() as session:
         expired_wishlists = await get_expired_wishlists(session, date_)
         for wishlist in expired_wishlists:
-            await make_wishlist_inactive(session, wishlist)
-            logging.info("Wishlist %s successfully deleted" % repr(wishlist))
+            async with session.begin_nested():
+                await make_wishlist_inactive(session, wishlist)
+                logging.info("Wishlist %s successfully deleted" % repr(wishlist))
