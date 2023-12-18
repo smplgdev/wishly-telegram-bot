504964df9df047e2d9ebac319720e2c49833561a
Ivan <smplgdev@gmail.com>
2023-12-18 22:10:39 +0100

    removed disable_web_page_preview=True

--- bot/bot_instance.py
+++ bot/bot_instance.py
@@ -4,6 +4,6 @@ from bot.config_reader import config
 
 bot = Bot(
     token=config.BOT_TOKEN.get_secret_value(),
-    disable_web_page_preview=True,
+    # disable_web_page_preview=True,
     parse_mode='HTML'
 )
